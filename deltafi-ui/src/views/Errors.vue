9140196196d649f22f267e5a8e1d58e2e65f38b4
Matthew Levine <Matthew.Levine@iNovexcorp.com>
2021-10-19 13:03:38 -0400

    [UI] Fixed Button

--- deltafi-ui/src/views/Errors.vue
+++ deltafi-ui/src/views/Errors.vue
@@ -2,34 +2,7 @@
   <div class="Error's">
     <h1>Welcome to the DeltaFi Error interface.</h1>
     <span></span>
-    <a>Standard Vue</a>
-    <table class="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th>DID (UUID)</th>
-          <th>Filename</th>
-          <th>Flow</th>
-          <th>Stage</th>
-          <th>Timestamp</th>
-          <th>Error Count</th>
-          <th>ReTry</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="error in errors" :key="error.did">
-          <td>{{ error.did }}</td>
-          <td>{{ error.filename }}</td>
-          <td>{{ error.flow }}</td>
-          <td>{{ error.stage }}</td>
-          <td>{{ error.created }}</td>
-          <td>{{ error.errors.length }}</td>
-          <td><button @click="RetryClickAction(error.did)">Retry</button></td>
-        </tr>
-      </tbody>
-    </table>
-    <div>
-      <a>Primevue</a>
-    </div>
+
       <DataTable
         :value="errors" 
         v-model:expandedRows="expandedRows"
@@ -42,7 +15,11 @@
         <Column field="stage" header="Stage"></Column>
         <Column field="created" header="Timestamp"></Column>
         <Column :expander="true"  field="errors.length" header="Error Count"></Column>
-        <Column field="did" header="Retry"></Column>
+        <Column :exportable="false" style="min-width:8rem">
+          <template #body="errors"> 
+            <button @click="RetryClickAction(errors.data.did)">Retry</button>
+          </template>
+        </Column>
         <template #expansion="errors">
           <div class="errors-Subtable">
             <DataTable :value="errors.data.errors" >
@@ -50,7 +27,7 @@
               <Column field="state" header="State"></Column>
               <Column field="created" header="Created"></Column>
               <Column field="cause" header="Cause"></Column>
-              <Column field="contex" header="Contex"></Column>
+              <Column field="context" header="Contex"></Column>
             </DataTable>
           </div>
       </template>
@@ -74,7 +51,7 @@ export default {
   },
   methods: {
     RetryClickAction( p_did ) {
-      alert('goofy' + p_did);
+      alert(p_did);
     }
   },
 };
