2ac4c75d8fe3b3bb4358bd11d55f72415ddc1ccd
Zak Ainsworth <zachary.l.ainsworth@systolic.com>
2021-10-20 19:28:56 +0000

    [UI] Fix issue with fetching API data from dev

--- deltafi-ui/src/App.vue
+++ deltafi-ui/src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/errors"> Error's</router-link>
+    <router-link to="/errors"> Errors</router-link>
   </div>
 <router-view/>
 </template>
--- deltafi-ui/src/views/Errors.vue
+++ deltafi-ui/src/views/Errors.vue
@@ -1,66 +1,70 @@
 <template>
-  <div class="Error's">
+  <div class="Errors">
     <h1>Welcome to the DeltaFi Error interface.</h1>
     <span></span>
 
-      <DataTable
-        :value="errors" 
-        v-model:expandedRows="expandedRows"
-        class="p-datatable-gridlines"
-      >
-
-        <Column field="did" header="DID (UUID)"> </Column>
-        <Column field="filename" header="Filename"></Column>
-        <Column field="flow" header="Flow"></Column>
-        <Column field="stage" header="Stage"></Column>
-        <Column field="created" header="Timestamp"></Column>
-        <Column :expander="true"  field="errors.length" header="Error Count"></Column>
-        <Column :exportable="false" style="min-width:8rem">
-          <template #body="errors"> 
-            <button @click="RetryClickAction(errors.data.did)">Retry</button>
-          </template>
-        </Column>
-        <template #expansion="errors">
-          <div class="errors-Subtable">
-            <DataTable :value="errors.data.errors" >
-              <Column field="action" header="Action"></Column>
-              <Column field="state" header="State"></Column>
-              <Column field="created" header="Created"></Column>
-              <Column field="cause" header="Cause"></Column>
-              <Column field="context" header="Contex"></Column>
-            </DataTable>
-          </div>
+    <DataTable
+      :value="errors"
+      v-model:expandedRows="expandedRows"
+      class="p-datatable-gridlines"
+    >
+      <Column field="did" header="DID (UUID)"> </Column>
+      <Column field="filename" header="Filename" :sortable="true"></Column>
+      <Column field="flow" header="Flow" :sortable="true"></Column>
+      <Column field="stage" header="Stage" :sortable="true"></Column>
+      <Column field="created" header="Timestamp" :sortable="true"></Column>
+      <Column
+        :expander="true"
+        field="errors.length"
+        header="Error Count"
+      ></Column>
+      <Column :exportable="false" style="min-width: 8rem">
+        <template #body="errors">
+          <button @click="RetryClickAction(errors.data.did)">Retry</button>
+        </template>
+      </Column>
+      <template #expansion="errors">
+        <div class="errors-Subtable">
+          <DataTable :value="errors.data.errors">
+            <Column field="action" header="Action"></Column>
+            <Column field="state" header="State"></Column>
+            <Column field="created" header="Created"></Column>
+            <Column field="cause" header="Cause"></Column>
+            <Column field="context" header="Contex"></Column>
+          </DataTable>
+        </div>
       </template>
-      </DataTable>
+    </DataTable>
   </div>
 </template>
 
 
 <script>
- import errors from "../test/errors.test.mock";
-let url = '/api/v1/errors';
-
-fetch(url).then(data => {
-return data.json();
-})
-.then(post => {
-console.log(post);
-});
- 
+// import errors from "../test/errors.test.mock";
 
 export default {
   name: "errors",
   data() {
     return {
-      errors,
+      errors: [],
       expandedRows: [],
-      
     };
   },
   methods: {
-    RetryClickAction( p_did ) {
+    RetryClickAction(p_did) {
       alert(p_did);
+    },
+    async fetchErrors() {
+      const request = new Request("/api/v1/errors", {
+        referrer: "",
+      });
+      const res = await fetch(request);
+      const data = await res.json();
+      this.errors = data.errors;
     }
+  },
+  created() {
+    this.fetchErrors();
   },
 };
 </script>
