d3f575843e22f9f536a611d2017ce6dcadeb12fd
Zak Ainsworth <zachary.l.ainsworth@systolic.com>
2021-10-27 00:57:37 +0000

    [UI] vue.config.js

--- deltafi-ui/vue.config.js
+++ deltafi-ui/vue.config.js
@@ -4,6 +4,9 @@ module.exports = {
     proxy: {
       '^/api': {
         target: process.env.DELTAFI_API_URL || 'https://dev.deltafi.org'
+      },
+      '^/graphql': {
+        target: process.env.DELTAFI_GATEWAY_URL || 'https://gateway.dev.deltafi.org'
       }
     }
   }
