19cb5b4f8c55d1a70a03b29f5c54dd1d1f63b104
Zak Ainsworth <zachary.l.ainsworth@systolic.com>
2021-12-13 20:26:23 +0000

    Resolve "Extend UI config to include links for dashboard"

--- deltafi-api/lib/deltafi/api/config/ui.rb
+++ deltafi-api/lib/deltafi/api/config/ui.rb
@@ -5,10 +5,20 @@ module Deltafi
     module Config
       module UI
         class << self
+          UI_CONFIGMAP = 'deltafi-ui-config'
+
           def config
+            ui_config = Deltafi::API.k8s_client.api('v1')
+                                    .resource('configmaps', namespace: NAMESPACE)
+                                    .get(UI_CONFIGMAP).data
+            links = YAML.safe_load(ui_config.links)
+
             {
-              domain: ENV['DELTAFI_UI_DOMAIN'] || 'example.deltafi.org',
-              title: ENV['DELTAFI_UI_TITLE'] || 'DeltaFi'
+              domain: ui_config.domain,
+              title: ui_config.title,
+              dashboard: {
+                links: links
+              }
             }
           end
         end
