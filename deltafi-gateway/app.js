0599a8845a881faef81f941c4c9fa27bf410767d
Zak Ainsworth <zachary.l.ainsworth@systolic.com>
2022-01-24 20:59:16 +0000

    Resolve "gateway should stop on SIGTERM"

--- deltafi-gateway/app.js
+++ deltafi-gateway/app.js
@@ -5,9 +5,11 @@ const { PORT = 4000 } = process.env;
 const { RETRY_DELAY = 5000 } = process.env;
 const { MAX_RETRIES = 5 } = process.env;
 
+var deltaFiGatewayServer;
+
 if (process.env.SERVICE_LIST === "null" || process.env.SERVICE_LIST === undefined) {
   console.log('Running in Kubernetes Service auto-discovery mode.')
-  const deltaFiGatewayServer = new DeltaFiGatewayServer(HOST, PORT, [], MAX_RETRIES, RETRY_DELAY);
+  deltaFiGatewayServer = new DeltaFiGatewayServer(HOST, PORT, [], MAX_RETRIES, RETRY_DELAY);
   await deltaFiGatewayServer.loadServiceListFromKubernetes();
   await deltaFiGatewayServer.start(true);
   deltaFiGatewayServer.watchKubernetesForServiceListChanges();
@@ -15,6 +17,12 @@ if (process.env.SERVICE_LIST === "null" || process.env.SERVICE_LIST === undefine
   console.log('Running in environment variable mode.');
   console.log(`SERVICE_LIST=${process.env.SERVICE_LIST}`);
   const SERVICE_LIST = JSON.parse(process.env.SERVICE_LIST);
-  const deltaFiGatewayServer = new DeltaFiGatewayServer(HOST, PORT, SERVICE_LIST, MAX_RETRIES, RETRY_DELAY);
+  deltaFiGatewayServer = new DeltaFiGatewayServer(HOST, PORT, SERVICE_LIST, MAX_RETRIES, RETRY_DELAY);
   deltaFiGatewayServer.start(true);
 }
+
+process.once('SIGTERM', async (code) => {
+  console.log('SIGTERM received...');
+  await deltaFiGatewayServer.stop();
+  process.exit();
+});
