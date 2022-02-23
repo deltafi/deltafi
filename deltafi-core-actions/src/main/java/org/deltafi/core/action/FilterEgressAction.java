834cb36d6c31f2f50ac4cddadaaf3fa1f559820a
Richard J Cresswell <richard.j.cresswell@systolic.com>
2022-02-23 14:55:13 +0000

    FILTER dropped egress

--- deltafi-core-actions/src/main/java/org/deltafi/core/action/FilterEgressAction.java
+++ deltafi-core-actions/src/main/java/org/deltafi/core/action/FilterEgressAction.java
@@ -2,14 +2,14 @@ package org.deltafi.core.action;
 
 import org.deltafi.actionkit.action.Result;
 import org.deltafi.actionkit.action.egress.EgressActionParameters;
-import org.deltafi.actionkit.action.egress.EgressResult;
 import org.deltafi.actionkit.action.egress.SimpleEgressAction;
+import org.deltafi.actionkit.action.filter.FilterResult;
 import org.deltafi.core.domain.api.types.ActionContext;
 import org.deltafi.core.domain.api.types.DeltaFile;
 
-public class DropEgressAction extends SimpleEgressAction {
+public class FilterEgressAction extends SimpleEgressAction {
     @Override
     public Result execute(DeltaFile deltaFile, ActionContext actionContext, EgressActionParameters params) {
-        return new EgressResult(actionContext, "dropped", 0);
+        return new FilterResult(actionContext, "filtered");
     }
 }
