da06457de8527a157485fc0d94b4b3171956d72f
Zak Ainsworth <zachary.l.ainsworth@systolic.com>
2022-02-26 02:58:59 +0000

    Resolve "Simplify core-domain k8s probe"

--- deltafi-core-domain/probe.sh
+++ deltafi-core-domain/probe.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 URL=http://127.0.0.1:8080/graphql
 TIMEOUT=5
-curl -m ${TIMEOUT} --connect-timeout ${TIMEOUT} -s --fail -o /dev/null ${URL}
+curl -m ${TIMEOUT} --connect-timeout ${TIMEOUT} -s -o /dev/null ${URL}
