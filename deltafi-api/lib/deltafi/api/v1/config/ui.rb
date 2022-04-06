d2cea0cb5348db4595233661549c64e1639e1de5
Zak Ainsworth <zachary.l.ainsworth@systolic.com>
2022-04-06 20:15:18 +0000

    Add top bar color to UI config

--- deltafi-api/lib/deltafi/api/v1/config/ui.rb
+++ deltafi-api/lib/deltafi/api/v1/config/ui.rb
@@ -11,16 +11,8 @@ module Deltafi
             def config
               ui_config = DF.k8s_client.api('v1')
                             .resource('configmaps', namespace: DF::Common::K8S_NAMESPACE)
-                            .get(UI_CONFIGMAP).data
-
-              return {
-                domain: ui_config.domain,
-                title: ui_config.title,
-                useUTC: ui_config.useUTC == 'true',
-                securityBanner: YAML.safe_load(ui_config.securityBanner) || { enabled: false },
-                externalLinks: YAML.safe_load(ui_config.externalLinks) || [],
-                deltaFileLinks: YAML.safe_load(ui_config.deltaFileLinks) || []
-              }
+                            .get(UI_CONFIGMAP).data.config
+              YAML.safe_load(ui_config)
             end
           end
         end
