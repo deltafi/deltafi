22ba7b4c29532bc9133908dda8162aad1f023a86
Zak Ainsworth <zachary.l.ainsworth@systolic.com>
2022-08-31 17:43:12 +0000

    Add Content Storage Check

---
+++ deltafi-api/lib/deltafi/monitor/status/checks/content_storage_check.rb
@@ -0,0 +1,62 @@
+#
+#    DeltaFi - Data transformation and enrichment platform
+#
+#    Copyright 2022 DeltaFi Contributors <deltafi@deltafi.org>
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+# frozen_string_literal: true
+
+require 'deltafi'
+
+module Deltafi
+  module Monitor
+    module Status
+      module Checks
+        class ContentStorageCheck < Status::Check
+          DEFAULT_THRESHOLD = 90
+          THRESHOLD_PROPERTY = 'deltafi.checks.contentStorage.percentThreshold'
+
+          def initialize
+            super('Content Storage Check')
+          end
+
+          def run
+            @threshold = threshold
+            @usage = usage
+
+            if @usage >= @threshold
+              self.code = 1
+              message_lines << "##### Content storage usage (#{@usage.round}%) is over the configured threshold (#{@threshold}%):\n"
+              message_lines << "\n_Threshold property: #{THRESHOLD_PROPERTY}_"
+            end
+
+            self
+          end
+
+          private
+
+          def threshold
+            (DF.system_properties[THRESHOLD_PROPERTY] || DEFAULT_THRESHOLD).to_i
+          end
+
+          def usage
+            content = DF::API::V1::Metrics::System.content
+            content[:usage] / content[:limit] * 100
+          end
+        end
+      end
+    end
+  end
+end
