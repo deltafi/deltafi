b8f071b133152d314fab9149ff32640fa3194e55
Jeremy Mayhew <jeremy.d.mayhew@systolic.com>
2023-01-13 10:03:00 -0500

    Update license header

--- deltafi-common/src/main/java/org/deltafi/common/types/Metric.java
+++ deltafi-common/src/main/java/org/deltafi/common/types/Metric.java
@@ -1,7 +1,7 @@
-/*
+/**
  *    DeltaFi - Data transformation and enrichment platform
  *
- *    Copyright 2022 DeltaFi Contributors <deltafi@deltafi.org>
+ *    Copyright 2021-2023 DeltaFi Contributors <deltafi@deltafi.org>
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 package org.deltafi.common.types;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
--- deltafi-egress-sink/probe.sh
+++ deltafi-egress-sink/probe.sh
@@ -2,7 +2,7 @@
 #
 #    DeltaFi - Data transformation and enrichment platform
 #
-#    Copyright 2022 DeltaFi Contributors <deltafi@deltafi.org>
+#    Copyright 2021-2023 DeltaFi Contributors <deltafi@deltafi.org>
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
