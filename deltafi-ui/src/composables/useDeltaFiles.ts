e9dcc8ea5b81190668b141a7142d5ca95676cd97
Zak Ainsworth <zachary.l.ainsworth@systolic.com>
2023-04-24 11:07:18 -0400

    Fixed bug with mediaType not being populated

--- deltafi-ui/src/composables/useDeltaFiles.ts
+++ deltafi-ui/src/composables/useDeltaFiles.ts
@@ -99,6 +99,7 @@ export default function useDeltaFiles() {
                 size: true,
               },
               size: true,
+              mediaType: true,
             }
           },
         },
