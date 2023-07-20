e848712c4f48ebb656cd86ba3e0bb046d5e46891
Richard Cresswell <richard.j.cresswell@systolic.com>
2023-07-20 06:09:03 -0400

    clean up exception handling in disk space delete

--- deltafi-core/src/main/java/org/deltafi/core/delete/DiskSpaceDelete.java
+++ deltafi-core/src/main/java/org/deltafi/core/delete/DiskSpaceDelete.java
@@ -44,35 +44,44 @@ public class DiskSpaceDelete extends DeletePolicyWorker {
     }
 
     public void run() {
+        DiskMetrics contentMetrics = null;
         try {
-            DiskMetrics contentMetrics = diskSpaceService.uncachedContentMetrics();
-
-            if (contentMetrics != null && contentMetrics.percentUsed() > maxPercent) {
-                log.info("Disk delete policy for " + (flow == null ? "all flows" : flow) + " executing: current used = " + contentMetrics.percentUsed() + "%, maximum = " + maxPercent + "%");
-                long bytesToDelete = contentMetrics.bytesOverPercentage(maxPercent);
-                log.info("Deleting at least " + bytesToDelete + " bytes");
-                while (bytesToDelete > 0) {
-                    List<DeltaFile> deleted = deltaFilesService.delete(bytesToDelete, flow, name, false);
-                    long bytesDeleted = deleted.stream().map(DeltaFile::getTotalBytes).reduce(0L, Long::sum);
-                    bytesToDelete -= bytesDeleted;
-                    log.info("Deleted batch of " + deleted.size() + " files, " + bytesDeleted + " bytes. Remaining: " + Math.max(0, bytesToDelete) + " bytes");
-                    if (deleted.isEmpty()) {
-                        log.warn("No DeltaFiles deleted -- disk is above threshold despite all content already being deleted.");
-                        break;
-                    }
-
-                    if (bytesToDelete > 0) {
-                        contentMetrics = diskSpaceService.uncachedContentMetrics();
-                        if (contentMetrics != null && contentMetrics.percentUsed() <= maxPercent) {
-                            log.info("Disk space delete batching stopped early due to disk usage below threshold.");
-                            break;
-                        }
-
-                    }
-                }
-            }
+            contentMetrics = diskSpaceService.uncachedContentMetrics();
         } catch (DeltafiApiException e) {
             log.warn("DeltaFi API is unreachable.  Unable to evaluate deletion criteria.");
+        }
+
+        if (contentMetrics == null || contentMetrics.percentUsed() <= maxPercent) {
+            return;
+        }
+
+        log.info("Disk delete policy for " + (flow == null ? "all flows" : flow) + " executing: current used = " + contentMetrics.percentUsed() + "%, maximum = " + maxPercent + "%");
+        long bytesToDelete = contentMetrics.bytesOverPercentage(maxPercent);
+        log.info("Deleting at least " + bytesToDelete + " bytes");
+        while (bytesToDelete > 0) {
+            List<DeltaFile> deleted = deltaFilesService.delete(bytesToDelete, flow, name, false);
+
+            long bytesDeleted = deleted.stream().map(DeltaFile::getTotalBytes).reduce(0L, Long::sum);
+            bytesToDelete -= bytesDeleted;
+
+            if (deleted.isEmpty()) {
+                log.warn("No DeltaFiles deleted -- disk is above threshold despite all content already being deleted.");
+                return;
+            }
+
+            log.info("Deleted batch of " + deleted.size() + " files, " + bytesDeleted + " bytes. Remaining: " + Math.max(0, bytesToDelete) + " bytes");
+
+            if (bytesToDelete > 0) {
+                try {
+                    contentMetrics = diskSpaceService.uncachedContentMetrics();
+                    if (contentMetrics != null && contentMetrics.percentUsed() <= maxPercent) {
+                        log.info("Disk space delete batching stopped early due to disk usage below threshold.");
+                        break;
+                    }
+                } catch (DeltafiApiException ignored) {
+                    // if the API is unreachable, continue deleting as planned
+                }
+            }
         }
     }
 }
