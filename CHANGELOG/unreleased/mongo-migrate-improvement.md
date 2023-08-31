00e5ac131e7d7904158088c2b4aca653ab210f0a
Jeremy Mayhew <jeremy.d.mayhew@systolic.com>
2023-08-31 13:19:08 -0400

    Mongo migration improvements

---
+++ CHANGELOG/unreleased/mongo-migrate-improvement.md
@@ -0,0 +1,25 @@
+# Changes on branch `mongo-migrate-improvement`
+Document any changes on this branch here.
+### Added
+- `deltafi mongo-migrate` now accepts `-f` flag to force migrations to run, as well as a list of migrations to run
+
+### Changed
+- `deltafi mongo-migrate` batches all migrations in a single exec to speed up the migration execution process
+
+### Fixed
+- 
+
+### Removed
+- 
+
+### Deprecated
+- 
+
+### Security
+- 
+
+### Tech-Debt/Refactor
+- 
+
+### Upgrade and Migration
+- 
--- migrations/runner/run-migrations.js
+++ migrations/runner/run-migrations.js
@@ -1,19 +1,42 @@
-const runMigration = (migrationName, migrationFile) => {
+function runMigration(migrationName, migrationFile) {
+  let start = performance.now();
+  load(migrationFile);
+  db.migrations.insertOne({name: migrationName, runAt: new Date()});
+  let timeTaken = (performance.now() - start)/1000;
+  print(`   -- Completed database migration ${migrationName}`);
+  print(`      Execution time: ${timeTaken.toFixed(3)} seconds`);
+}
+
+function runMigrationOnce(migrationName, migrationFile) {
     var migrationRan = db.migrations.findOne({"name": migrationName});
     if (!migrationRan) {
-        load(migrationFile);
-        db.migrations.insertOne({name: migrationName, runAt: new Date()});
-        print(` -- Completed database migration ${migrationName}`);
+        runMigration(migrationName, migrationFile)
     } else {
-        print(` -- Skipped migration ${migrationName} as it has already been run`);
+        print(`   -- Skipped migration ${migrationName} as it has already been run`);
     }
 }
 
-migrationName = process.env.MIGRATION_NAME
-migrationFile = process.env.MIGRATION_FILE
-
-if (!migrationName || !migrationFile) {
-    print(`Missing migration name ${migrationName} or file ${migrationFile}`);
-} else {
-    runMigration(migrationName, migrationFile)
+function getJSFileNames(directory) {
+    const files = fs.readdirSync(directory);
+    const jsFiles = files.filter(file => path.extname(file) === '.js');
+    return jsFiles;
 }
+
+const directory = process.env.MIGRATION_PATH;
+const files = process.env.MIGRATION_FILES;
+const force = (process.env.MIGRATION_FORCE !== undefined)
+
+let jsFiles = getJSFileNames(directory);
+
+if (files !== undefined) jsFiles = files.split(' ');
+
+print(`Running ${jsFiles.length} migrations:`);
+
+jsFiles.forEach((file, index) => {
+  const filePath = path.join(directory, file);
+  const fileName = path.basename(file);
+
+  print(`   -- Running database migration ${fileName} (${index + 1})`);
+  force ? runMigration(fileName, filePath) : runMigrationOnce(fileName, filePath);
+  print('');
+});
