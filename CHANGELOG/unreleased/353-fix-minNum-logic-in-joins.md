b79b5ce0c52ecc21a0d5570a30ca43f041da696a
Micah McCarty <micah.p.mccarty@systolic.com>
2024-10-09 01:04:56 +0000

    Fix minNum logic in joins

---
+++ CHANGELOG/unreleased/353-fix-minNum-logic-in-joins.md
@@ -0,0 +1,26 @@
+# Changes on branch `353-fix-minNum-logic-in-joins`
+Document any changes on this branch here.
+### Added
+- 
+
+### Changed
+- 
+
+### Fixed
+- Fixed bug of minNum and maxNum not able to be equal.
+- Fixed bug of minNum being able to equal to 0. 
+
+### Removed
+- 
+
+### Deprecated
+- 
+
+### Security
+- 
+
+### Tech-Debt/Refactor
+- 
+
+### Upgrade and Migration
+- 
--- deltafi-ui/src/components/flowBuilder/ActionConfigurationDialog.vue
+++ deltafi-ui/src/components/flowBuilder/ActionConfigurationDialog.vue
@@ -29,30 +29,30 @@
         </Message>
       </div>
       <dl>
-        <template v-for="displayActionInfo of getDisplayValues(rowdata)" :key="displayActionInfo">
+        <template v-for="displayActionInfo of getDisplayValues(rowData)" :key="displayActionInfo">
           <template v-if="displayFieldTest(displayActionInfo)"> </template>
           <template v-else>
             <dt>{{ displayMap.get(displayActionInfo).header }}</dt>
             <dd v-if="_.isEqual(displayMap.get(displayActionInfo).type, 'string')">
-              <InputText v-model="rowdata[displayActionInfo]" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowdata.disableEdit" />
+              <InputText v-model="rowData[displayActionInfo]" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowData.disableEdit" />
             </dd>
             <dd v-else-if="_.isEqual(displayMap.get(displayActionInfo).type, 'object')">
-              <template v-if="Array.isArray(rowdata[displayActionInfo])">
-                <template v-if="rowdata[displayActionInfo].includes('any')">
-                  <DialogTemplate component-name="plugin/ListEdit" :model-value="rowdata[displayActionInfo]" :header="`Add New ${displayMap.get(displayActionInfo).header}`" dialog-width="25vw">
+              <template v-if="Array.isArray(rowData[displayActionInfo])">
+                <template v-if="rowData[displayActionInfo].includes('any')">
+                  <DialogTemplate component-name="plugin/ListEdit" :model-value="rowData[displayActionInfo]" :header="`Add New ${displayMap.get(displayActionInfo).header}`" dialog-width="25vw">
                     <div class="p-inputtext p-component">
-                      <div v-for="item in rowdata[displayActionInfo]" :key="item" class="list-item">{{ item }}</div>
+                      <div v-for="item in rowData[displayActionInfo]" :key="item" class="list-item">{{ item }}</div>
                     </div>
                   </DialogTemplate>
                 </template>
                 <template v-else>
-                  <div v-for="item in rowdata[displayActionInfo]" :key="item" class="list-item">{{ item }}</div>
+                  <div v-for="item in rowData[displayActionInfo]" :key="item" class="list-item">{{ item }}</div>
                 </template>
               </template>
               <template v-if="_.isEqual(displayActionInfo, 'schema')">
                 <div class="deltafi-fieldset">
                   <div class="px-2 pt-3">
-                    <JsonForms :data="data" :renderers="renderers" :uischema="uischema" :schema="rowdata['schema']" @change="onChange($event, rowdata)" />
+                    <JsonForms :data="data" :renderers="renderers" :uischema="uiSchema" :schema="rowData['schema']" @change="onChange($event, rowData)" />
                   </div>
                 </div>
               </template>
@@ -61,19 +61,19 @@
                   <div class="px-2 pt-3">
                     <dt>maxAge</dt>
                     <dd>
-                      <InputText v-model="joinData['maxAge']" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowdata.disableEdit" />
+                      <InputText v-model="joinData['maxAge']" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowData.disableEdit" />
                     </dd>
                     <dt>minNum</dt>
                     <dd>
-                      <InputNumber v-model="joinData['minNum']" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowdata.disableEdit" :min="0" show-buttons />
+                      <InputNumber v-model="joinData['minNum']" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowData.disableEdit" :min="0" show-buttons />
                     </dd>
-                    <dt>maxNumber</dt>
+                    <dt>maxNum</dt>
                     <dd>
-                      <InputNumber v-model="joinData['maxNum']" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowdata.disableEdit" :min="0" show-buttons />
+                      <InputNumber v-model="joinData['maxNum']" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowData.disableEdit" :min="0" show-buttons />
                     </dd>
                     <dt>metadataKey</dt>
                     <dd>
-                      <InputText v-model="joinData['metadataKey']" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowdata.disableEdit" />
+                      <InputText v-model="joinData['metadataKey']" class="inputWidth" :disabled="displayMap.get(displayActionInfo).disableEdit && rowData.disableEdit" />
                     </dd>
                   </div>
                 </div>
@@ -108,7 +108,7 @@ import _ from "lodash";
 const { rendererList, myStyles } = usePrimeVueJsonSchemaUIRenderers();
 provide("style", myStyles);
 const renderers = ref(Object.freeze(rendererList));
-const uischema = ref(undefined);
+const uiSchema = ref(undefined);
 const isMounted = ref(useMounted());
 const emit = defineEmits(["updateAction"]);
 const props = defineProps({
@@ -137,9 +137,9 @@ const errors = ref([]);
 
 const { actionIndexProp: actionIndex, closeDialogCommand } = reactive(props);
 
-const rowdata = reactive(JSON.parse(JSON.stringify(props.rowDataProp)));
-const data = ref(_.isEmpty(_.get(rowdata, "parameters", null)) ? {} : rowdata["parameters"]);
-const joinData = ref(_.isEmpty(_.get(rowdata, "join", null)) ? defaultJoinTemplate.value : rowdata["join"]);
+const rowData = reactive(JSON.parse(JSON.stringify(props.rowDataProp)));
+const data = ref(_.isEmpty(_.get(rowData, "parameters", null)) ? {} : rowData["parameters"]);
+const joinData = ref(_.isEmpty(_.get(rowData, "join", null)) ? defaultJoinTemplate.value : rowData["join"]);
 
 const originalJoinData = JSON.parse(JSON.stringify(joinData.value));
 
@@ -169,7 +169,7 @@ const getDisplayValues = (obj) => {
 
 const displayFieldTest = (displayActionInfo) => {
   let fieldsCheck = ["requiresDomains", "requiresEnrichments", "requiresMetadataKeyValues"];
-  return (fieldsCheck.includes(displayActionInfo) && _.isEmpty(rowdata[displayActionInfo])) || (_.isEqual(displayActionInfo, "schema") && _.isEmpty(_.get(rowdata, "schema.properties", null)));
+  return (fieldsCheck.includes(displayActionInfo) && _.isEmpty(rowData[displayActionInfo])) || (_.isEqual(displayActionInfo, "schema") && _.isEmpty(_.get(rowData, "schema.properties", null)));
 };
 
 const hasErrors = computed(() => {
@@ -208,8 +208,14 @@ const submit = async () => {
       newJoin["maxNum"] = joinData.value["maxNum"];
     }
 
+    if (_.isNumber(joinData.value["minNum"])) {
+      if (_.lte(joinData.value["minNum"], 0)) {
+        errors.value.push("minNum cannot be 0 or less");
+      }
+    }
+
     if (_.isNumber(joinData.value["minNum"]) && _.isNumber(joinData.value["maxNum"])) {
-      if (!_.lt(joinData.value["minNum"], joinData.value["maxNum"])) {
+      if (!_.lte(joinData.value["minNum"], joinData.value["maxNum"])) {
         errors.value.push("minNum cannot be greater than maxNum");
       }
     }
@@ -227,11 +233,11 @@ const submit = async () => {
   }
 
   if (!_.isEmpty(newJoin)) {
-    rowdata["join"] = newJoin;
+    rowData["join"] = newJoin;
   }
 
   closeDialogCommand.command();
-  emit("updateAction", { actionIndex: actionIndex, updatedAction: rowdata });
+  emit("updateAction", { actionIndex: actionIndex, updatedAction: rowData });
 };
 </script>
 
