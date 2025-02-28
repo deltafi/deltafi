7724a567c2befc4e1428a215312e3ca579e4c1c5
Richard Cresswell <richard.j.cresswell@systolic.com>
2025-02-28 07:25:36 -0500

    add if exists to migration

--- deltafi-core/src/main/resources/db/migration/V17__index_optimizations.sql
+++ deltafi-core/src/main/resources/db/migration/V17__index_optimizations.sql
@@ -1,9 +1,9 @@
 -- add did to the index so the query doesn't have to hit the table
-DROP INDEX idx_delta_files_content_deleted_created_data_source;
+DROP INDEX IF EXISTS idx_delta_files_content_deleted_created_data_source;
 CREATE INDEX idx_delta_files_content_deleted_created_data_source ON delta_files (created, data_source, did) WHERE (content_deleted IS NOT NULL);
 
 -- indexes were not hitting because of explicit state enum cast
-DROP INDEX idx_delta_file_flows_requeue;
+DROP INDEX IF EXISTS idx_delta_file_flows_requeue;
 CREATE INDEX idx_delta_file_flows_requeue ON delta_file_flows (modified, delta_file_id) WHERE state = 'IN_FLIGHT' AND cold_queued = FALSE;
-DROP INDEX idx_delta_file_flows_error_count;
+DROP INDEX IF EXISTS idx_delta_file_flows_error_count;
 CREATE INDEX idx_delta_file_flows_error_count ON delta_file_flows (delta_file_id) WHERE state = 'ERROR' AND error_acknowledged IS NULL;
