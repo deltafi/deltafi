4ac6f3b5eba2c643dc6cc723ff42ea4716750e9f
Jeremy Mayhew <jeremy.d.mayhew@systolic.com>
2025-11-13 00:33:22 -0500

    Base image updates

--- tui/internal/orchestration/orchestrator.go
+++ tui/internal/orchestration/orchestrator.go
@@ -44,16 +44,16 @@ type Orchestrator interface {
 type valuesData struct {
 	Tag                     string
 	Repo                    string `default:"deltafi"`
-	GrafanaContainer        string `default:"deltafi/grafana:12.2.0-0"`
+	GrafanaContainer        string `default:"deltafi/grafana:12.2.1-0"`
 	GraphiteContainer       string `default:"graphiteapp/graphite-statsd:1.1.10-5"`
-	LokiContainer           string `default:"grafana/loki:2.9.15"`
-	MinioContainer          string `default:"deltafi/minio:RELEASE.2025-10-15T17-29-55Z-1"`
-	NginxContainer          string `default:"nginx:1.29.1-alpine3.22"`
-	PromtailContainer       string `default:"grafana/promtail:2.9.15"`
+	LokiContainer           string `default:"grafana/loki:2.9.17"`
+	MinioContainer          string `default:"deltafi/minio:RELEASE.2025-10-15T17-29-55Z-2"`
+	NginxContainer          string `default:"nginx:1.29.3-alpine3.22"`
+	PromtailContainer       string `default:"grafana/promtail:3.5.8"`
 	ValkeyContainer         string `default:"valkey/valkey:9.0.0-alpine"`
-	DockerWebGuiContainer   string `default:"deltafi/docker-web-gui:1.0.2-3"`
-	PostgresContainer       string `default:"deltafi/timescaledb:2.19.3-pg16-0"`
-	PostgresLookupContainer string `default:"postgres:16.9-alpine3.22"`
+	DockerWebGuiContainer   string `default:"deltafi/docker-web-gui:1.0.2-4"`
+	PostgresContainer       string `default:"deltafi/timescaledb:2.19.3-pg16-2"`
+	PostgresLookupContainer string `default:"postgres:16.10-alpine3.22"`
 	JavaIDEContainer        string `default:"deltafi/deltafi-java-dev:jdk21-gradle8.5-2"`
 	JavaDevContainer        string `default:"deltafi/devcontainer-java:jdk21-gradle8.5-0"`
 }
