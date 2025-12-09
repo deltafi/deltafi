8ede4f7a565302caa0cb88ade3785bdd530cb829
Zak Ainsworth <zachary.l.ainsworth@systolic.com>
2025-12-09 14:40:38 -0500

    Fixed bug in documentation generation

---
+++ CHANGELOG/unreleased/show-version-in-docs.md
@@ -0,0 +1,25 @@
+# Changes on branch `show-version-in-docs`
+Document any changes on this branch here.
+### Added
+-
+
+### Changed
+-
+
+### Fixed
+- Fixed bug in documentation generation
+
+### Removed
+-
+
+### Deprecated
+-
+
+### Security
+-
+
+### Tech-Debt/Refactor
+-
+
+### Upgrade and Migration
+-
--- deltafi-docs/.vitepress/config.js
+++ deltafi-docs/.vitepress/config.js
@@ -6,7 +6,6 @@ import fs from "fs";
 import { resolve, basename } from "path";
 import fg from "fast-glob";
 import { parse } from "csv-parse/sync";
-import { execSync } from "child_process";
 
 function isHtmlTag(tag) {
   // Remove angle brackets
@@ -20,7 +19,17 @@ function isHtmlTag(tag) {
   return htmlTags.has(tag);
 }
 
-function getLatestVersion(changelogPath) {
+function globalReplace(fileNamePattern, searchValue, replaceValue) {
+  const files = fg.sync(resolve(__dirname, fileNamePattern));
+  files.forEach((file) => {
+    let content = fs.readFileSync(file, 'utf8');
+    content = content.replace(searchValue, replaceValue);
+    fs.writeFileSync(file, content);
+  });
+}
+
+function getLatestVersion() {
+  const changelogPath = resolve(__dirname, "../docs/CHANGELOG.md");
   const content = fs.readFileSync(changelogPath, "utf8");
   const match = content.match(/^## \[(\d+\.\d+\.\d+)\]/m);
   if (!match) throw new Error("No version found in CHANGELOG.md");
@@ -55,14 +64,9 @@ function copyDocs() {
 }
 
 function updateVersionInDocs() {
-  const version = getLatestVersion(resolve(__dirname, "../docs/CHANGELOG.md"));
+  const version = getLatestVersion();
   console.log("Replacing __VERSION__ with", version, "in built docs...");
-  const files = fg.sync(resolve(__dirname, './dist/**/*.{js,html}'));
-  files.forEach((file) => {
-    let content = fs.readFileSync(file, 'utf8');
-    content = content.replace(/__VERSION__/g, version);
-    fs.writeFileSync(file, content);
-  });
+  globalReplace('./dist/**/*.{js,html}', /__VERSION__/g, version);
 }
 
 function injectPermissionsInDocs() {
@@ -76,10 +80,12 @@ function injectPermissionsInDocs() {
     permissionsTable += `<tr><td>${record.Name}</td><td>${record.Category}</td><td>${record.Description}</td></tr>`;
   });
   permissionsTable += "</table>"; // Close table
-  const file = resolve(__dirname, './dist/config/authentication.html');
-  let content = fs.readFileSync(file, 'utf8');
-  content = content.replace(/PERMISSIONS_TABLE/g, permissionsTable);
-  fs.writeFileSync(file, content);
+  globalReplace('./dist/**/*.{js,html}', /PERMISSIONS_TABLE/g, permissionsTable);
+}
+
+function cleanupChangelog() {
+  console.log("Cleaning up changelog");
+  globalReplace('./dist/**/*.{js,html}', /\[(\d+\.\d+\.\d+)\]/g, "$1");
 }
 
 function generateDocsSidebar() {
@@ -133,13 +139,13 @@ export default defineConfig(({ mode }) => {
       // https://vitepress.dev/reference/default-theme-config
       nav: [
         {
-          text: "Home",
-          link: "/",
-        },
-        {
-          text: "deltafi.org",
-          link: "https://deltafi.org",
-        },
+          text: `v${getLatestVersion()}`,
+          items: [
+            { text: 'Changelog', link: '/CHANGELOG' },
+            { text: 'Contributing', link: '/contributing' },
+            { text: 'v1.2.20', link: 'https://v1.docs.deltafi.org/' }
+          ]
+        }
       ],
       search: {
         provider: "local",
@@ -156,10 +162,21 @@ export default defineConfig(({ mode }) => {
           },
           link: "https://gitlab.com/deltafi/deltafi",
         },
+        {
+          icon: {
+            svg: `
+            <svg viewBox="0 0 512 512" id="_x30_1" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M418.275,146h-46.667 c-5.365-22.513-12.324-43.213-20.587-61.514c15.786,8.776,30.449,19.797,43.572,32.921C403.463,126.277,411.367,135.854,418.275,146 z M452,256c0,17.108-2.191,33.877-6.414,50h-64.034c1.601-16.172,2.448-32.887,2.448-50s-0.847-33.828-2.448-50h64.034 C449.809,222.123,452,238.892,452,256z M256,452c-5.2,0-21.048-10.221-36.844-41.813c-6.543-13.087-12.158-27.994-16.752-44.187 h107.191c-4.594,16.192-10.208,31.1-16.752,44.187C277.048,441.779,261.2,452,256,452z M190.813,306 c-1.847-16.247-2.813-33.029-2.813-50s0.966-33.753,2.813-50h130.374c1.847,16.247,2.813,33.029,2.813,50s-0.966,33.753-2.813,50 H190.813z M60,256c0-17.108,2.191-33.877,6.414-50h64.034c-1.601,16.172-2.448,32.887-2.448,50s0.847,33.828,2.448,50H66.414 C62.191,289.877,60,273.108,60,256z M256,60c5.2,0,21.048,10.221,36.844,41.813c6.543,13.087,12.158,27.994,16.752,44.187H202.404 c4.594-16.192,10.208-31.1,16.752-44.187C234.952,70.221,250.8,60,256,60z M160.979,84.486c-8.264,18.301-15.222,39-20.587,61.514 H93.725c6.909-10.146,14.812-19.723,23.682-28.593C130.531,104.283,145.193,93.262,160.979,84.486z M93.725,366h46.667 c5.365,22.513,12.324,43.213,20.587,61.514c-15.786-8.776-30.449-19.797-43.572-32.921C108.537,385.723,100.633,376.146,93.725,366z M351.021,427.514c8.264-18.301,15.222-39,20.587-61.514h46.667c-6.909,10.146-14.812,19.723-23.682,28.593 C381.469,407.717,366.807,418.738,351.021,427.514z"></path></g>
+            </svg>
+          `,
+          },
+          link: "https://deltafi.org/",
+        },
+
       ],
       footer: {
         message: '<a href="https://deltafi.org/#contact">Contact US</a>',
-        copyright: "Copyright © 2022-2025 DeltaFi. All rights reserved.",
+        copyright: "Copyright © 2021-2025 DeltaFi. All rights reserved.",
       },
     },
     markdown: {
@@ -261,6 +278,7 @@ export default defineConfig(({ mode }) => {
     buildEnd() {
       updateVersionInDocs();
       injectPermissionsInDocs();
+      cleanupChangelog();
     }
   };
 });
